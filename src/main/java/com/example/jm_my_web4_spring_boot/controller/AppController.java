314e94456f21c646c91dac4235159075f62c7ac2
EfanRu <ffgeniy@mail.ru>
2020-01-31 17:35:33 +0300

    Stating RESTfull project

--- src/main/java/com/example/jm_my_web4_spring_boot/controller/AppController.java
+++ src/main/java/com/example/jm_my_web4_spring_boot/controller/AppController.java
@@ -10,39 +10,44 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.List;
 
-@Controller
+//@Controller
+@RestController
 public class AppController {
     private static final Logger LOG = LoggerFactory.getLogger(AppController.class);
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+//    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+    @GetMapping({"/", "/login"})
+//    public String login(Principal principal) {
     public String login(Principal principal) {
         return principal == null ? "login" : "redirect:/user";
     }
 
-    @RequestMapping(value = "/admin/add", method = RequestMethod.POST)
-    public String add(HttpServletRequest req) {
-        userService.addUser(new User(
-                req.getParameter("firstName"),
-                req.getParameter("lastName"),
-                req.getParameter("login"),
-                req.getParameter("password"),
-                Long.parseLong(req.getParameter("phoneNumber")),
-                new Role(req.getParameter("role"))));
-        return "redirect:/admin/all";
+//    @RequestMapping(value = "/admin/add", method = RequestMethod.POST)
+    @PostMapping("/users")
+//    public String add(HttpServletRequest req) {
+    public String add(@RequestBody User user) {
+//        userService.addUser(new User(
+//                req.getParameter("firstName"),
+//                req.getParameter("lastName"),
+//                req.getParameter("login"),
+//                req.getParameter("password"),
+//                Long.parseLong(req.getParameter("phoneNumber")),
+//                new Role(req.getParameter("role"))));
+        userService.addUser(user);
+        return "redirect:/admin";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+//    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
     public String user(ModelMap model) {
         Authentication auth;
         try {
@@ -57,36 +62,44 @@ public class AppController {
         return "user";
     }
 
-    @RequestMapping(value = "/admin/all", method = RequestMethod.GET)
+//    @RequestMapping(value = "/admin/all", method = RequestMethod.GET)
+    @GetMapping("/admin")
     public String allUser(ModelMap model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("listUser", users);
         return "adminAll";
     }
 
-    @RequestMapping(value = "/admin/edit", method = RequestMethod.GET)
+//    @RequestMapping(value = "/admin/edit", method = RequestMethod.GET)
+    @GetMapping("/admin/edit")
     public String editUserPage(@ModelAttribute("id") String id, ModelMap model) {
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "editUsers";
     }
 
-    @RequestMapping(value = "/admin/edit", method = RequestMethod.POST)
-    public String editUser(/*@ModelAttribute("user") User user,*/ HttpServletRequest req) {
-        userService.updateUser(
-                req.getParameter("id"),
-                req.getParameter("firstName"),
-                req.getParameter("lastName"),
-                req.getParameter("phoneNumber"),
-                req.getParameter("role"),
-                req.getParameter("login"),
-                req.getParameter("password"));
-        return "redirect:/admin/all";
+//    @RequestMapping(value = "/admin/edit", method = RequestMethod.POST)
+    @PutMapping(value = "/admin/edit/{id}")
+//    public String editUser(/*@ModelAttribute("user") User user,*/ HttpServletRequest req) {
+    public String editUser(@RequestBody User user) {
+        userService.updateUser(user);
+//        userService.updateUser(
+//                req.getParameter("id"),
+//                req.getParameter("firstName"),
+//                req.getParameter("lastName"),
+//                req.getParameter("phoneNumber"),
+//                req.getParameter("role"),
+//                req.getParameter("login"),
+//                req.getParameter("password"));
+//        return "redirect:/admin/all";
+        return "redirect:/admin";
     }
 
-    @RequestMapping(value = "/admin/delete", method = RequestMethod.GET)
-    public String delUser(@ModelAttribute("id") String id) {
+//    @RequestMapping(value = "/admin/delete", method = RequestMethod.GET)
+    @DeleteMapping("/admin/{id}")
+    public String delUser(@PathVariable String id) {
         userService.deleteUser(id);
-        return "redirect:/admin/all";
+//        return "redirect:/admin/all";
+        return "redirect:/admin";
     }
 }
