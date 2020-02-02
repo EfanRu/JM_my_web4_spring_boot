19026716a61473d826e9845c67ad30674814e156
Vyacheslav <ffgeniy@mail.ru>
2020-02-02 19:51:36 +0300

    Some changes

--- src/main/java/com/example/jm_my_web4_spring_boot/controller/AppController.java
+++ src/main/java/com/example/jm_my_web4_spring_boot/controller/AppController.java
@@ -1,8 +1,8 @@
-package com.example.jm_my_web4_spring_boot.controller;
+package com.example.springboot.controller;
 
-import com.example.jm_my_web4_spring_boot.model.Role;
-import com.example.jm_my_web4_spring_boot.model.User;
-import com.example.jm_my_web4_spring_boot.service.UserService;
+import com.example.springboot.model.Role;
+import com.example.springboot.model.User;
+import com.example.springboot.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
@@ -27,14 +28,24 @@ public class AppController {
 //    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
     @GetMapping({"/", "/login"})
 //    public String login(Principal principal) {
-    public String login(Principal principal) {
-        return principal == null ? "login" : "redirect:/user";
+//    public String login(Principal principal) {
+//        return principal == null ? "login" : "redirect:/user";
+//    }
+    public ModelAndView login(Principal principal) {
+        ModelAndView mav = new ModelAndView();
+//        return principal == null ? "redirect:/login" : "redirect:/user";
+        if (principal == null) {
+            mav.setViewName("login");
+        } else {
+            mav.setViewName("user");
+        }
+        return mav;
     }
 
 //    @RequestMapping(value = "/admin/add", method = RequestMethod.POST)
     @PostMapping("/users")
 //    public String add(HttpServletRequest req) {
-    public String add(@RequestBody User user) {
+    public User addUser(@RequestBody User user) {
 //        userService.addUser(new User(
 //                req.getParameter("firstName"),
 //                req.getParameter("lastName"),
@@ -43,7 +54,9 @@ public class AppController {
 //                Long.parseLong(req.getParameter("phoneNumber")),
 //                new Role(req.getParameter("role"))));
         userService.addUser(user);
-        return "redirect:/admin";
+        return user;
+//        userService.addUser(user);
+//        return "redirect:/admin";
     }
 
 //    @RequestMapping(value = "/user", method = RequestMethod.GET)
@@ -64,24 +77,28 @@ public class AppController {
 
 //    @RequestMapping(value = "/admin/all", method = RequestMethod.GET)
     @GetMapping("/admin")
-    public String allUser(ModelMap model) {
-        List<User> users = userService.getAllUsers();
-        model.addAttribute("listUser", users);
-        return "adminAll";
+//    public String allUser(ModelMap model) {
+    public List<User> allUser() {
+        return userService.getAllUsers();
+//        List<User> users = userService.getAllUsers();
+//        model.addAttribute("listUser", users);
+//        return "adminAll";
     }
 
 //    @RequestMapping(value = "/admin/edit", method = RequestMethod.GET)
     @GetMapping("/admin/edit")
-    public String editUserPage(@ModelAttribute("id") String id, ModelMap model) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
-        return "editUsers";
+//    public String editUserPage(@ModelAttribute("id") String id, ModelMap model) {
+    public User editUserPage(@ModelAttribute("id") String id) {
+//        User user = userService.getUserById(id);
+        return userService.getUserById(id);
+//        model.addAttribute("user", user);
+//        return "editUsers";
     }
 
 //    @RequestMapping(value = "/admin/edit", method = RequestMethod.POST)
     @PutMapping(value = "/admin/edit/{id}")
 //    public String editUser(/*@ModelAttribute("user") User user,*/ HttpServletRequest req) {
-    public String editUser(@RequestBody User user) {
+    public User editUser(@RequestBody User user) {
         userService.updateUser(user);
 //        userService.updateUser(
 //                req.getParameter("id"),
@@ -92,14 +109,13 @@ public class AppController {
 //                req.getParameter("login"),
 //                req.getParameter("password"));
 //        return "redirect:/admin/all";
-        return "redirect:/admin";
+        return user;
     }
 
 //    @RequestMapping(value = "/admin/delete", method = RequestMethod.GET)
     @DeleteMapping("/admin/{id}")
-    public String delUser(@PathVariable String id) {
+    public void delUser(@PathVariable String id) {
         userService.deleteUser(id);
 //        return "redirect:/admin/all";
-        return "redirect:/admin";
     }
 }
