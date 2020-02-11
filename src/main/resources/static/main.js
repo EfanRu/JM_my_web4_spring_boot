78cd65b8ee2aeeefd317eabafbbc2f8ec57b4400
EfanRu <ffgeniy@mail.ru>
2020-02-11 13:08:23 +0300

    Working project. Deleting don't working code

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
 
-    clean_user_table();
     refresh_user_table();
 
-    // $("button[data-toggle=modal]").click(function() {
     $(document).on('click', 'button[data-toggle=modal]', function() {
         var id = $(this).attr('id');
 
@@ -37,7 +35,6 @@ $(document).ready(function () {
                     '                                                </select>\n' +
                     '                                                <br>\n' +
                     '                                                <div class="form-group text-center">\n' +
-                    '\n' +
                     '                                                    <b>Login:</b><br>\n' +
                     '                                                    <input class="input-lg" type="text" name="login" value="' + user.login + '" id="editLogin"><br>\n' +
                     '                                                    <b>Password:</b><br>\n' +
@@ -57,85 +54,21 @@ $(document).ready(function () {
     });
 
     $("#addUser").submit(function (event) {
-
-        //stop submit the form, we will post it manually.
         event.preventDefault();
-
         add_new_user_submit();
-        // clean_user_table();
-        // refresh_user_table();
     });
 
-    // $('.editFromClass').on('submit', function (event) {
     $(document).on('click', '.editFromClassBut', function (event) {
-
         event.preventDefault();
-
-        // var editUserId = $(this).attr('id');
-        // var editUserId2 = $(this).val();
-        // $('#' + editUserId).on(edit_user_submit(editUserId));
         edit_user_submit();
-
-        $('#modal-warning').close();
-        // clean_user_table();
-        // refresh_user_table();
     });
 
 
-    // $(".deleteFromClass").on('click', function (event) {
     $(document).on('click', ".deleteFromClass", function (event) {
-
-        //stop submit the form, we will post it manually.
         event.preventDefault();
-
         var delUserId = $(this).attr('id');
         $('#' + delUserId).on(delete_user_submit(delUserId));
-
-        // clean_user_table();
-        // refresh_user_table();
     });
-
-    function update_table() {
-        // $("#allUsers").after(function () {
-        //     var id = $("#idAllUsers").val();
-            var id = 0;
-        //     var id = $(this).val();
-            $.ajax({
-                url: '/admin/all',
-                type: 'GET',
-                dataType: "json",
-                success: function (users) {
-                    var data = '';
-                    // var user = value[id];
-
-                    // var userIndex = $('#userIndex').val();
-                    // var userIndex = $(this).val();
-                    $.each(users, function (index, user) {
-                    //     if (userIndex === value.id) {
-                    data += '<td>' + user.id + '</td>';
-                    data += '<td>' + user.firstName + '</td>';
-                    data += '<td>' + user.lastName + '</td>';
-                    data += '<td>' + user.login + '</td>';
-                    data += '<td>' + user.phoneNumber + '</td>';
-                    data += '<td>' + user.role.name + '</td>';
-                    // }
-                    });
-                    // $('#allUsers tr').remove();
-                    $('#idAllUsers').append(data);
-                },
-                error: function (e) {
-                    console.log("ERROR : ", e);
-                }
-            })
-
-        // });
-    }
-
-
-        function clean_user_table() {
-            $('#user_table tr').remove();
-        }
-
 
         function refresh_user_table() {
             $.ajax({
@@ -153,15 +86,10 @@ $(document).ready(function () {
                         data += '<td>' + user.phoneNumber + '</td>';
                         data += '<td>' + user.role.name + '</td>';
                         data += '<td>' +
-                            // '                                                <form action="/admin" method="delete" id="deleteUserHid">\n' +
-                            // '                                                    <button class="btn btn-danger btn-xs hidden" type="submit" name="id" value="' + user.id + '"><span class="glyphicon glyphicon-trash"></span></button>\n' +
-                            // '                                                </form>\n' +
-                            // '\n' +
                             '                                                <form class="deleteFromClass" action="/admin" method="delete" id="' + user.id + '">\n' +
                             '                                                    <button class="btn btn-danger btn-xs" type="button" name="id" value="' + user.id + '" id="delId"><span class="glyphicon glyphicon-trash"></span></button>\n' +
                             '                                                </form>\n' +
                             '                                            </td>\n' +
-                            '\n' +
                             '                                            <td>\n' +
                             '                                                <form class="editFromClass" id="' + user.id + '">\n' +
                             '                                                    <button href="#modal-warning" type="button" class="btn btn-primary btn-xs" data-toggle="modal" id="' + user.id + '">edit</button>\n' +
@@ -188,74 +116,7 @@ $(document).ready(function () {
                             '                                            </td>\n' +
                             '                                        </tr>\n';
 
-
-
-
-
-
-
-                        //     '<form action="/admin" method="delete" id="deleteUserHid">\n' +
-                        //     '<button class="btn btn-danger btn-xs hidden" type="submit" name="id" value=' + user.id + ' id="delId"><span class="glyphicon glyphicon-trash"></span></button>\n' +
-                        //     '</form>' +
-                        //     '<form action="/admin" method="delete" id="deleteUser">\n' +
-                        //     '<button class="btn btn-danger btn-xs" type="submit" name="id" value=' + user.id + ' id="delId"><span class="glyphicon glyphicon-trash"></span></button>\n' +
-                        //     '</form>' +
-                        //     '' +'</td>';
-                        // data += '<td>' + '' +
-                        //     '<button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#modal-warning" attr="data-target=#modal-warning' + user.id + '">edit</button>\n' +
-                        //     '                            <div class="modal modal-warning fade in" id="modal-warning' + user.id + '">\n' +
-                        //     '                                <div class="modal-dialog">\n' +
-                        //     '                                    <div class="modal-content">\n' +
-                        //     '                                        <div class="modal-header">\n' +
-                        //     '                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">\n' +
-                        //     '                                                <span aria-hidden="true">×</span></button>\n' +
-                        //     '                                            <h4 class="modal-title">Edit user</h4>\n' +
-                        //     '                                        </div>\n' +
-                        //     '                                        <div class="modal-body">\n' +
-                        //     '                                            <form action="/admin" method="put" id="editUser">\n' +
-                        //     '                                                <div class="form-group text-center">\n' +
-                        //     '                                                    <b>Id:</b><br>\n' +
-                        //     '                                                    <input class="input-lg" type="text" name="id" value="' + user.id + '" id="editId' + user.id + '"><br>\n' +
-                        //     '                                                    <b>First name:</b><br>\n' +
-                        //     '                                                    <input class="input-lg" type="text" name="firstName" value="' + user.firstName + '" id="editFirstName' + user.id + '"><br>\n' +
-                        //     '                                                    <b>Last name:</b><br>\n' +
-                        //     '                                                    <input class="input-lg" type="text" name="lastName" value="' + user.lastName + '" id="editLastName' + user.id + '"><br>\n' +
-                        //     '                                                    <b>Phone number:</b><br>\n' +
-                        //     '                                                    <input class="input-lg" type="text" name="phoneNumber" value="' + user.phoneNumber + '" id="editPhoneNumber' + user.id + '"><br>\n' +
-                        //     '                                                </div>\n' +
-                        //     '                                                <div class="text-center"><b>Role</b>\n' +
-                        //     '                                                </div>\n' +
-                        //     '                                                <select class="selectpicker" data-live-search="true" data-live-search-style="startsWith" name = "role" id="editRole' + user.id + '">\n' +
-                        //     '                                                    <option value="admin">admin</option>\n' +
-                        //     '                                                    <option value="user">user</option>\n' +
-                        //     '                                                </select>\n' +
-                        //     '                                                <br>\n' +
-                        //     '                                                <div class="form-group text-center">\n' +
-                        //     '\n' +
-                        //     '                                                    <b>Login:</b><br>\n' +
-                        //     '                                                    <input class="input-lg" type="text" name="login" value="' + user.login +'" id="editLogin' + user.id + '"><br>\n' +
-                        //     '                                                    <b>Password:</b><br>\n' +
-                        //     '                                                    <input class="input-lg" type="password" name="password" id="editPassword' + user.id + '">\n' +
-                        //     '                                                    <br>\n' +
-                        //     '\n' +
-                        //     '                                                    <div class="modal-footer">\n' +
-                        //     '                                                        <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>\n' +
-                        //     '                                                        <button type="submit" class="btn btn-outline">Change</button>\n' +
-                        //     '                                                    </div>\n' +
-                        //     '                                                </div>\n' +
-                        //     '                                            </form>\n' +
-                        //     '                                        </div>\n' +
-                        //     '                                    </div>\n' +
-                        //     '                                </div>\n' +
-                        //     '                            </div>';
-                        //     '' +'</td>';
-                        // data += '</tr>';
-
-                        // data += '</table>\n';
-
                     });
-                    // $('#user_table tr').remove();
-                    // $('#user_table').append(data);
                     $('#user_table').html(data);
                 },
                 error: function (e) {
@@ -284,7 +145,6 @@ $(document).ready(function () {
                 timeout: 600000,
                 success: function (data) {
                     console.log("SUCCESS : ", data);
-                    clean_user_table();
                     refresh_user_table();
                 },
                 error: function (e) {
@@ -317,7 +177,6 @@ $(document).ready(function () {
                 timeout: 600000,
                 success: function (data) {
                     console.log("SUCCESS : ", data);
-                    clean_user_table();
                     refresh_user_table();
                     $('#modal-warning').close();
                 },
@@ -337,12 +196,11 @@ $(document).ready(function () {
                 dataType: 'json',
                 success: function (result) {
                     console.log("SUCCESS : ", result);
-                    clean_user_table();
+                    // clean_user_table();
                     refresh_user_table();
                 },
                 error: function (e) {
                     console.log("ERROR : ", e);
-                    clean_user_table();
                     refresh_user_table();
                 }
             });
